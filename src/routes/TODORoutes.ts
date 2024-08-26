a8b7671dbf69a6a28434a135a105a7a7af923937
joaosp7 <joaosp7@gmail.com>
2024-08-26 06:00:09 -0300

    added some http routes

--- src/routes/TODORoutes.ts
+++ src/routes/TODORoutes.ts
@@ -1,11 +1,54 @@
 
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { knex } from 'knex';
 import { z } from 'zod';
-import { randomUUID } from "node: crypto";
+import * as fs from 'node:fs'
+import { randomUUID } from "node:crypto";
+
+interface Todo{
+    id: string,
+    name: string,
+    description: string,
+    finished: boolean,
+}
+const todos: Array<Todo> = [ ];
 
 export async function TODORoutes(app: FastifyInstance){
-    app.get('/', ()=>{
-        return 'Wellcome to my TODO!';
+    app.get('/', (request: FastifyRequest, reply: FastifyReply)=>{
+        return todos
     })
+
+    app.get('/:id', (request: FastifyRequest, reply: FastifyReply) => {
+       const { id } = request.params
+        todos.forEach(todo => {
+            const found_it = id == todo.id
+
+            if (found_it){
+
+            reply.send(todo) 
+            }
+
+        });
+    // Run thought the todos list and find the specific ID that i am looking for
+    })
+
+    app.post('/', (request:FastifyRequest, reply: FastifyReply) =>{
+       const { name, description } = request.body;
+       const todo: Todo = {
+        id: randomUUID(),
+        name,
+        description,
+        finished: false
+    }
+        todos.push(todo);
+        reply.status(201).send('TODO Created!')
+    })
+
+    // GET SPECIF TODO
+
+    // CREATE A TODO
+
+    // DELETE A TODO
+
+    // UPDATE A TODO
 }
